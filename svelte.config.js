c893ddefc0c3f187edbc822607185b6a4347c811
Philipp R. Proksch <33249058+prpcodes@users.noreply.github.com>
2024-11-23 02:56:37 +0100

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +11,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/learntogrow' : '',
-        }
+		adapter: adapter()
 	}
 };
 
 export default config;
+
+
+
