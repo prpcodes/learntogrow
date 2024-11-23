0b01c0850d8d6f2127c128d6d5e28130124d7ff1
Philipp R. Proksch <33249058+prpcodes@users.noreply.github.com>
2024-11-23 03:51:45 +0100

    added times

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,24 @@
 	let water = $derived(() => plants * 4);
 	let bags = $derived(() => plants * 44 / 5);
 	let raw = $derived(() => plants * 44);
-  
 
-  
+
+  // watering is every four hours, 3 times, starting at the time the first plant was planted
+  let start_time = $state('00:00');
+  let watering = $derived(() => {
+    let times = [];
+    let time = new Date();
+    time.setHours(parseInt(start_time.split(':')[0]));
+    time.setMinutes(parseInt(start_time.split(':')[1]));
+    for (let i = 0; i < 3; i++) {
+      times.push(new Date(time.getTime() + i * 4 * 60 * 60 * 1000));
+    }
+    return times;
+  });
+
+
 </script>
+
 
 <svelte:head>
 	<title>Page</title>
@@ -16,10 +30,12 @@
 
 <div class="card w-96">
 	<div class="card-body">
-		<h2 class="card-title">Wie viele Pflanzen?</h2>
 		<div class="card-actions justify-end">
-			<div class="flex w-full flex-col">
+			<div class="flex w-full flex-col gap-4">
+        <label for="plants">Wie viele Pflanzen?</label>
 				<input type="text" name="plants" bind:value={plants} class="input input-bordered w-full" />
+        <label for="time">Wann wurde die erste Angepflanzt?</label>
+        <input type="time" name="time" class="input input-bordered w-full" bind:value={start_time} />
 			</div>
 		</div>
 	</div>
@@ -68,6 +84,33 @@
             <tr>
               <td>{raw()}</td>
               <td>{bags()}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    
+		</div>
+	</div>
+</div>
+
+<div class="card w-96">
+	<div class="card-body">
+		<h2 class="card-title">Uhrzeiten</h2>
+		<div class="card-actions justify-end">
+      <div class="overflow-x-auto w-full">
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="w-1/3">1. Gießen</th>
+              <th class="w-1/3">2. Gießen</th>
+              <th class="w-1/3">Ernten</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+               {#each watering() as time}
+                <td>{time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</td>
+              {/each}
             </tr>
           </tbody>
         </table>
